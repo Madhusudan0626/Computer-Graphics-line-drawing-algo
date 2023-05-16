b36f48f3536fff8c0a6c8798b298402940a9bbe1
Madhusudan Chand <87716301+Madhusudan0626@users.noreply.github.com>
2023-05-16 09:25:50 +0530

    Add files via upload

---
+++ 3D/test.c
@@ -0,0 +1,67 @@
+#include <stdio.h>  
+#include <GL/gl.h>  
+#include <GL/glut.h>  
+
+#include <GL/glut.h>
+
+void drawAxes()
+{
+    // X-axis
+    glColor3f(1.0f, 0.0f, 0.0f); // Red
+    glBegin(GL_LINES);
+    glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(1.0f, 0.0f, 0.0f);
+    glEnd();
+
+    // Y-axis
+    glColor3f(0.0f, 1.0f, 0.0f); // Green
+    glBegin(GL_LINES);
+    glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 1.0f, 0.0f);
+    glEnd();
+
+    // Z-axis
+    glColor3f(0.0f, 0.0f, 1.0f); // Blue
+    glBegin(GL_LINES);
+    glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, 1.0f);
+    glEnd();
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    gluLookAt(2.0, 2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+    drawAxes();
+
+    glFlush();
+}
+
+void reshape(int width, int height)
+{
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0, (double)width / height, 1.0, 10.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("OpenGL Axes");
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+
+    glEnable(GL_DEPTH_TEST);
+
+    glutMainLoop();
+
+    return 0;
+}
